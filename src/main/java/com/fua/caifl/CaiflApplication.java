b08746c1654d3c1d9351e37e94c84b8e0e0ad1bc
Nick_Jackson <15213432067@163.com>
2021-01-11 14:42:54 +0800

    初次提交

---
+++ src/main/java/com/fua/caifl/CaiflApplication.java
@@ -0,0 +1,17 @@
+package com.fua.caifl;
+/**
+ * 软件工程蔡枫林个人博客网站
+ */
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaiflApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaiflApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fua/caifl/CaiflApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fua.caifl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaiflApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
