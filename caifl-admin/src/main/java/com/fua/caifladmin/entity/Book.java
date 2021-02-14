ea99640de33ac32da4d6f7112f2410c9cce8e810
Nick_Jackson <15213432067@163.com>
2021-02-14 19:58:07 +0800

    整合博客后台管理系统

---
+++ caifl-admin/src/main/java/com/fua/caifladmin/entity/Book.java
@@ -0,0 +1,87 @@
+package com.fua.caifladmin.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * (Book)实体类
+ *
+ * @author makejava
+ * @since 2020-03-22 23:36:10
+ */
+//@Data
+public class Book implements Serializable {
+    private static final long serialVersionUID = -37395183798253996L;
+    
+    private Integer id;
+    
+    private String bookname;
+    
+    private String pub;
+    
+    private Double price;
+    
+    private Date date;
+    
+    private Integer count;
+    
+    private String kind;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBookname() {
+        return bookname;
+    }
+
+    public void setBookname(String bookname) {
+        this.bookname = bookname;
+    }
+
+    public String getPub() {
+        return pub;
+    }
+
+    public void setPub(String pub) {
+        this.pub = pub;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+}
--- caifl-admin/src/test/java/com/fua/caifladmin/CaiflAdminApplicationTests.java
+++ caifl-admin/src/test/java/com/fua/caifladmin/CaiflAdminApplicationTests.java
@@ -1,13 +1,32 @@
 package com.fua.caifladmin;
 
+import com.fua.caifladmin.entity.User;
+import com.fua.caifladmin.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @SpringBootTest
 class CaiflAdminApplicationTests {
 
+    @Resource
+    private UserService userService;
+
     @Test
     void contextLoads() {
+    }
+
+
+    @Test
+    void testLogin(){
+        User caifl = userService.queryByName("caifl");
+        System.out.println("hello world!");
+        System.out.println(caifl.toString());
+//        List<User> users = userService.queryList();
+//        System.out.println("hello world!");
+//        System.out.println(users);
     }
 
 }
